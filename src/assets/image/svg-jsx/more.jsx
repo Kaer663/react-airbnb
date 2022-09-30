b684e152edb5cd7321f9fd00acbd62cbbbcdf18b
caizhun <632496308@qq.com>
2022-09-30 18:35:52 +0800

    feat上传到 gitee

---
+++ src/assets/image/svg-jsx/more.jsx
@@ -0,0 +1,23 @@
+import React, { memo } from "react";
+import parser from "style-to-object";
+
+export default memo(function global() {
+  return (
+    <svg
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="presentation"
+      focusable="false"
+      style={parser(
+        "display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 3; overflow: visible;"
+      )}
+    >
+      <g fill="none" fill-rule="nonzero">
+        <path d="m2 16h28"></path>
+        <path d="m2 24h28"></path>
+        <path d="m2 8h28"></path>
+      </g>
+    </svg>
+  );
+});
--- src/components/app-header/cnps/header-right/index.jsx
+++ src/components/app-header/cnps/header-right/index.jsx
@@ -5,6 +5,12 @@ import { RightWrapper } from "./style";
 export default memo(function InLeft() {
   return (
     <RightWrapper>
+      <div className="btn">
+        <span>登录</span>
+        <span>注册</span>
+        <span>图标</span>
+      </div>
+      <div className="profile"></div>
       <h2>Right 这是一个计算属性 </h2>
     </RightWrapper>
   );
